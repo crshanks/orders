55573b6bee916d5a1013b2fe1c9db2f128002361
Craig Shanks <cshanks@newrelic.com>
2020-11-19 17:39:26 +0000

    Name docker image sock-shop-orders-nr

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 IMAGE=orders
 
-set -ev
+set -v
 
 SCRIPT_DIR=$(dirname "$0")
 
@@ -25,8 +25,9 @@ CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 $DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
 
-cp -r $CODE_DIR/docker $CODE_DIR/target/docker/
+cp -r $CODE_DIR/docker $CODE_DIR/target
 cp -r $CODE_DIR/target/*.jar $CODE_DIR/target/docker/${IMAGE}
 
-REPO=${GROUP}/${IMAGE}
+# REPO=${GROUP}/${IMAGE}
+REPO=${GROUP}/sock-shop-orders-nr
     $DOCKER_CMD build -t ${REPO}:${COMMIT} $CODE_DIR/target/docker/${IMAGE};
